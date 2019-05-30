45e2af134b1b25cf18d4d34fb1f3e74838f32d03
0639323113 <piyamon41321@gmail.com>
2019-05-30 11:37:25 +0700

    [add] new project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("ตอง\n");
+        
+        System.out.print("แอน บั๊มส์\n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("รส ซากุระ \n");
+        
+        System.out.print("อยู่บรรทัดใหม่ละ");
+        
+    }
+}
